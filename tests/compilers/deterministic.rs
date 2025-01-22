c0e7b4af1f4b1a0fe2e841c4d29077b2761de6b2
nickgs1337 <nicklatim@live.com>
2025-01-22 20:12:58 -0300

    Make pointer copy panics be treated as errors

--- tests/compilers/deterministic.rs
+++ tests/compilers/deterministic.rs
@@ -1,5 +1,6 @@
+use std::panic::AssertUnwindSafe;
 use anyhow::Result;
-use wasmer::{wat2wasm, Module};
+use wasmer::{imports, Module, Store, wat2wasm, Singlepass, Universal};
 
 fn compile_and_compare(wasm: &[u8]) -> Result<()> {
     let store = Default::default();
@@ -41,4 +42,28 @@ fn deterministic_table() -> Result<()> {
     )?;
 
     compile_and_compare(&wasm_bytes)
+}
+
+#[test]
+fn should_not_panic_on_bad_init_param() -> Result<()> {
+    let wasm_bytes = wat2wasm(br#"
+(module
+    (type $t0 (func (param i32)))
+    (func $init (type $t0) (param $p0 i32))
+    (memory $memory 2)
+    (export "memory" (memory 0))
+    (export "init" (func $init)))
+"#,)?;
+
+    let store = Store::new(&Universal::new(Singlepass::default()).engine());
+    let module = Module::new(&store, wasm_bytes)?;
+
+    match wasmer::Instance::new(&module, &imports! {}) {
+        Ok(_) => panic!("Expected an error"),
+        Err(e) => {
+            assert_eq!(e.to_string(), "RuntimeError: invalid pointer copy");
+        }
+    }
+
+    Ok(())
 }
